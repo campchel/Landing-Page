b94b2489f8aead3b281e56ff7a4661ff5bb1eaf5
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-04-26 11:41:41 -0600

    update 2

---
+++ Landing_Page.js
@@ -0,0 +1,10 @@
+function hover(object){
+    object.src ="./Static/logot.png"
+    object.classList.add("logo2")
+    console.log(object)
+}
+function hoverOut(object){
+    object.src ="./Static/bestlogo.png"
+    object.classList.remove("logo2")
+    console.log(object)
+}
